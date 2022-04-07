7468052805f0f2f7beb451bc33146bc49e6e78e9
Dylan Wetterling <dwetterling@my.wctc.edu>
2022-04-07 00:55:50 -0500

    Allowed user to add a post to a blog, and view all posts in either all blogs or a blog they choose

--- Program.cs
+++ Program.cs
@@ -23,6 +23,8 @@ namespace BlogsConsole
                     // Display all Blogs from the database
                     var db = new BloggingContext();
                     var query = db.Blogs.OrderBy(b => b.Name);
+                    
+                    
 
                 foreach (var item in query)
                 {
@@ -40,17 +42,97 @@ namespace BlogsConsole
                 logger.Info("Blog added - {name}", name);
 
                 } else if (selection == 3){
-
+                    
+                    Console.WriteLine("Please select a Blog to post to");
+                    var db = new BloggingContext();
+                    var query = db.Blogs.OrderBy(b => b.BlogId);
+                     foreach (var item in query)
+                {
+                    Console.WriteLine(item.BlogId +". " + item.Name);
+                }
+                   var input = Console.ReadLine(); //get the input
+                    int choice;
+                    if (!int.TryParse(input, out choice))
+                            {
+                                Console.WriteLine("Not an integer");
+                            }
+                            
+                            else {
+                                    choice = Convert.ToInt32(input);
+                                    int max = query.Count();
+                                    
+                                    if (choice > max) {
+                                        Console.WriteLine("Not a valid Blog Id");
+                                    }
+                                    else {
+                                        Console.WriteLine("Enter the Post title");
+                                        var title = Console.ReadLine();
+                                        if (title.Length == 0){
+                                            Console.WriteLine("Must enter a Title");
+                                        } else{
+                                            Console.WriteLine("Enter Post content");
+                                            var content = Console.ReadLine();
+                                            
+                                            var post = new Post {Title = title, Content = content, BlogId = choice };
+                                            db.AddPost(post);
+                                            logger.Info("Post added - {title}", title);
+                                        }
+                                        
+                                    }
+                            }
+  
                 } else if (selection == 4){
                      var db = new BloggingContext();
-                     var query = db.Posts.OrderBy(b => b.Title);
+                     var count = db.Posts.Count();
+
+                     Console.WriteLine("What Blog would you like to view the posts of?\n0). All Blogs\n");
+                     var query = db.Blogs.OrderBy(b => b.BlogId);
+                     foreach (var item in query)
+                {
+                    Console.WriteLine(item.BlogId +". " + item.Name);
                 }
-                
+                   var input = Console.ReadLine(); //get the input
+                    int choice;
+                    if (!int.TryParse(input, out choice))
+                            {
+                                Console.WriteLine("Not an integer");
+                            }
+                            
+                            else{
+                            
+                             choice = Convert.ToInt32(input);
+                             int max = query.Count();
+                                if (choice == 0){
+                                     Console.WriteLine("Number of posts: " + count);
+                                     var query2 = db.Posts.OrderBy(b => b.PostId);
+                                 foreach (var item in query2)
+                                    {
+                                        Console.WriteLine("Blog: " + item.Blog.Name);
+                                        Console.WriteLine("Title: " + item.Title);
+                                        Console.WriteLine("Content: " + item.Content);
+                                    }
+                                    }else if(choice > 0 & 0 < max){
+                                        
+                                        var query3 = db.Posts.Where(b => b.BlogId == choice);
+                                        var count2 = query3.Count();
+                                        Console.WriteLine("Number of posts: " + count2);
+                                        foreach (var item in query3)
+                                    {
+                                    Console.WriteLine("Blog: " + item.Blog.Name);
+                                        Console.WriteLine("Title: " + item.Title);
+                                        Console.WriteLine("Content: " + item.Content);
+                                    }
+
+                     }
+                     else if (choice > max ) {
+                      Console.WriteLine("Not a valid Blog Id");
+                     }
+      
+                }
+                }
 
             }while(selection != 5);  
             
-            
-
             logger.Info("Program ended");
         }
     }
